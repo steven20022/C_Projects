26b22f78df87eebc3e500b1d26ee8c3e48e4bbdb
steven20022 <jacobelam11@gmail.com>
2022-09-29 10:43:11 -0400

    9/29/22

---
+++ App.s
@@ -0,0 +1,30 @@
+	.file	"App.c"
+	.text
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+.LC0:
+	.ascii "Hello World!!!\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+.LFB80:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	call	__main
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rcx
+	call	puts
+	movl	$0, %eax
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (Rev1, Built by MSYS2 project) 12.2.0"
+	.def	puts;	.scl	2;	.type	32;	.endef
